f17885a24f79efeffd5318863f457ca00f2ceb03
gozaltech <beqaprogger@gmail.com>
2020-05-11 16:46:16 +0400

    Initial commit

---
+++ addon/appModules/unigram.py
@@ -0,0 +1,33 @@
+﻿#appModules/unigram.py
+# Copyright (C) 2020 beqa gozalishvili <beqaprogger@gmail.com>
+#This file is covered by the GNU General Public License.
+#See the file COPYING for more details.
+
+import addonHandler
+import api
+import appModuleHandler
+import controlTypes
+from NVDAObjects.UIA import UIA
+import ui
+
+addonHandler.initTranslation()
+
+class AppModule(appModuleHandler.AppModule):
+
+	def event_NVDAObject_init(self, obj):
+		if not isinstance(obj, UIA): return
+		try:
+			if obj.UIAElement.CachedClassName == 'Hyperlink' and obj.UIAElement.CachedAutomationId == 'Button' and obj.next.role == controlTypes.ROLE_PROGRESSBAR:
+				obj.name = _("Play")
+			if obj.UIAElement.CachedClassName == 'ToggleButton':
+				return
+			if obj.role == controlTypes.ROLE_LISTITEM:
+				if obj.parent.UIAElement.CurrentAutomationId == "Messages":
+					return
+				elif obj.parent.UIAElement.CurrentAutomationId == "ScrollingFiles" or obj.parent.UIAElement.CurrentAutomationId == "ScrollingLinks" or obj.parent.UIAElement.CurrentAutomationId == "ScrollingHost" or obj.parent.UIAElement.CurrentAutomationId == "ScrollingMedia" or obj.parent.UIAElement.CurrentAutomationId == "ScrollingMusic" or obj.parent.UIAElement.CurrentAutomationId == "ScrollingVoice" or obj.parent.UIAElement.CurrentAutomationId == "DialogsSearchListView" or obj.lastChild.UIAElement.CurrentAutomationId == "Label":
+					name = []
+					for i in obj.children:
+						name.append(i.name)
+					obj.name = ", ".join(name)
+		except AttributeError:
+			pass
---
+++ readme.md
@@ -0,0 +1,5 @@
+# NVDA Add-on for Unigram application #
+
+Copyright (C) 2020 Beqa Gozalishvili.
+
+This addon make a unigram (telegram client for windows 10) more accessible by correcting information in several listviews and labeling unlabeled controls.
